cab074ee8d2749bf09b5fcdfae13199a7db85692
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-22 23:43:15 +0530

    easy exercises😤😡

--- js/forloop.js
+++ js/forloop.js
@@ -1,19 +1,38 @@
-// 1 to 10
+//1. 1 to 10
 /*for (let i = 0; i <= 10; i++) {
     console.log(i);
 } */
-// 2 to 20 even numbers
-for (let i = 0; i <=20; i+=2) {
+//2. 2 to 20 even numbers
+/*for (let i = 0; i <=20; i+=2) {
     console.log(i);
+}*/
+//3. for that returns sum of numbers from 1 to 100
+/*let sum = 0;
+for (let i = 0; i <=100; i++) {
+    sum += i;
 }
-
-
-
-
-
-
-
-
+console.log("total sum = " + sum); */
+//4. write a for loop that iterates over an array and prints each element
+/*let myArray = ["car1", "car2", "car3", "car4","car5"];
+for (let i = 0; i < myArray.length; i++) {
+    console.log(myArray[i]);
+} 
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]); */
+//5. to calculate factorial of numbers from 1 to 10
+/*let number = Math.floor(Math.random()*10 + 1);
+//console.log(number);
+let factorial = 1;
+for (let i = 1; i <= number; i++) {
+    factorial *= i;
+}
+console.log(`number = ${number}`);
+console.log(`factorial = ${factorial}`); */
+////////////////////////////////////////////////////////////////////
+// bit hard exercises now on😤😤😡😡
 
 
 

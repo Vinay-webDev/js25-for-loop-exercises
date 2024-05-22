c2c1cc6a8bdf94ba5adbc2708bcc2277c62fc81d
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-05-22 23:21:54 +0530

    1.

---
+++ js/forloop.js
@@ -0,0 +1,26 @@
+// 1 to 10
+/*for (let i = 0; i <= 10; i++) {
+    console.log(i);
+} */
+// 2 to 20 even numbers
+for (let i = 0; i <=20; i+=2) {
+    console.log(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
